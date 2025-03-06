fe6d8bd610f45a14693ee12e8c9e0a715f467a68
Jorge <jorge.garcia.celis@gmail.com>
2025-03-06 12:32:55 -0600

    [Change] Matrix paints from top to buttom

--- src/Matrix.jsx
+++ src/Matrix.jsx
@@ -23,33 +23,18 @@ function Matrix({ options, points }) { //Recibe los puntos y el largo de la matr
     return (
         <StyledMatrix>
             <div>
-                {Array.from({ length: options.yLength + 1 }, (_, rowIndex) => (
+                {Array.from({ length: options.yLength + 2 }, (_, rowIndex) => (
+                    
                     <Block
                         key={rowIndex}
-                        number={options.yLength - rowIndex}
+                        number={rowIndex === 0 ? null : rowIndex - 1}
                         backgroundColor={'blue'}
                         margin={rowIndex === 0 ? '.5px 0 0 0' : '1px 0'}
                     />
                 ))}
             </div>
             <Plane width={(options.xLength + 1) * 21}>
-                {Array.from({ length: options.yLength + 1 }, (_, rowIndex) => (
-                    <Row key={rowIndex}>
-                        {Array.from(
-                            { length: options.xLength + 1 },
-                            (_, colIndex) => {
-                                const isPoint = points.some(point => point[0] === colIndex && point[1] === (options.yLength - rowIndex));
-                                return (
-                                    <Block
-                                        key={`${rowIndex}-${colIndex}`}
-                                        backgroundColor={isPoint ? options.color : null} // Pinta el bloque si es un punto
-                                    />
-                                );
-                            }
-                        )}
-                    </Row>
-                ))}
-                <Row> {/* Crea la fila de numeros de abajo */}
+            <Row>
                     {Array.from(
                         { length: options.xLength + 1 },
                         (_, colIndex) => (
@@ -61,6 +46,23 @@ function Matrix({ options, points }) { //Recibe los puntos y el largo de la matr
                         )
                     )}
                 </Row>
+                {Array.from({ length: options.yLength + 1 }, (_, rowIndex) => (
+                    <Row key={rowIndex}>
+                        {Array.from(
+                            { length: options.xLength + 1 },
+                            (_, colIndex) => {
+                                const isPoint = points.some(point => point[0] === colIndex && point[1] === rowIndex);
+                                return (
+                                    <Block
+                                        key={`${rowIndex}-${colIndex}`}
+                                        backgroundColor={isPoint ? options.color : null} // Pinta el bloque si es un punto
+                                    />
+                                );
+                            }
+                        )}
+                    </Row>
+                ))}
+
             </Plane>
         </StyledMatrix>
     );
